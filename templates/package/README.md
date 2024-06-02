f03792accee89643f231655d0f57a69a19d2223d
Bastien Gatellier <contact@bgatellier.fr>
2024-06-02 16:52:58 +0200

    feat: add new module creation CLI

---
+++ templates/package/README.md
@@ -0,0 +1,9 @@
+---
+force: true
+---
+
+# Heart {{ name | capitalize }}
+
+_Heart {{ name | capitalize }}_ is a template module which goal is to serve as a codebase for new module creation.
+
+To create a new module, see the _[Create a new module](../../docs/CREATE_NEW_MODULE.md)_ guide.
--- templates/package/src/[name|pascal_case]Module.ts
+++ templates/package/src/[name|pascal_case]Module.ts
@@ -1,6 +1,12 @@
+{% set class_name = name ~ 'Module' | pascal_case %}
+
+---
+to: src//{{ class_name }}.ts
+---
+
 import { Module, logger, type ModuleMetadata } from "@fabernovel/heart-common"
 
-export class TplModule extends Module {
+export class {{ class_name }} extends Module {
   constructor(moduleMetadata: ModuleMetadata, verbose: boolean) {
     super(moduleMetadata, verbose)
 
