96a388178431e57e8b63bbc7ccb38064fef75290
Bastien Gatellier <contact@bgatellier.fr>
2024-06-29 01:23:53 +0200

    fix: template

--- templates/package/src/[name|pascal_case]Module.ts
+++ templates/package/src/[name|pascal_case]Module.ts
@@ -1,17 +1,17 @@
 {% set class_name = name ~ 'Module' | pascal_case %}
 
 ---
-to: src//{{ class_name }}.ts
+to: src/{{ class_name }}.ts
 ---
 
-import { Module, logger, type ModuleMetadata } from "@fabernovel/heart-common"
+import { Module, type ModuleMetadata, logger } from "@fabernovel/heart-common";
 
 export class {{ class_name }} extends Module {
-  constructor(moduleMetadata: ModuleMetadata, verbose: boolean) {
-    super(moduleMetadata, verbose)
-
-    if (verbose) {
-      logger.info(`${moduleMetadata.name} initialized.`)
-    }
-  }
+	constructor(moduleMetadata: ModuleMetadata, verbose: boolean) {
+		super(moduleMetadata, verbose):
+		
+		if (verbose) {
+			logger.info(`${moduleMetadata.name} initialized.`);
+		}
+	}
 }
