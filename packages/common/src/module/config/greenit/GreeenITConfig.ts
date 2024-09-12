9fd4b472faba3c98c79afe4c16b5d97e243f8039
Bastien Gatellier <contact@bgatellier.fr>
2024-09-12 23:09:57 +0200

    feat: only the url option is required

--- packages/common/src/module/config/greenit/GreeenITConfig.ts
+++ packages/common/src/module/config/greenit/GreeenITConfig.ts
@@ -2,8 +2,8 @@ import type { Options } from "greenit-cli";
 import type { Config } from "../Config.js";
 
 // use Exclude instead of Pick to allow future new options.
-// the goal is to be as close as possible the dependency API.
+// the goal is to be as close as possible of the dependency API.
 export type GreenITConfig = Config &
-	Exclude<Options, "ci" | "max_tab"> & {
+	Partial<Exclude<Options, "ci" | "max_tab">> & {
 		url: string;
 	};
