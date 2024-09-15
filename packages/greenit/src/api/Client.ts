bd19584059ac54ad484789ba6e76a0444b9b35b6
Bastien Gatellier <contact@bgatellier.fr>
2024-09-15 21:35:08 +0200

    chore: replace native subprocess execution by execa call

--- packages/greenit/src/api/Client.ts
+++ packages/greenit/src/api/Client.ts
@@ -1,8 +1,8 @@
-import { spawnSync } from "node:child_process";
 import { readFileSync } from "node:fs";
 import { join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { type GreenITConfig, type GreenITReport, logger } from "@scodi/common";
+import { execa } from "execa";
 import {
 	type JSONReport,
 	type Options,
@@ -27,32 +27,11 @@ export async function requestResult(
 	config: GreenITConfig,
 	verbose: boolean,
 ): Promise<GreenITReport["result"]> {
-	// get the browser executable path from the puppeteer dependency of the greenit-cli dependency
-	// https://github.com/puppeteer/puppeteer/issues/679#issuecomment-1274988821
-	const { stdout, error } = spawnSync(
-		"node",
-		["-e", "console.log(require('puppeteer').executablePath())"],
-		{
-			// go to the greenit-cli dependency directory, and run in it the command above
-			cwd: join(__dirname, "../../node_modules/greenit-cli"),
-			encoding: "utf-8",
-		},
-	);
-
-	if (error !== undefined) {
-		return Promise.reject(
-			new GreenITError(`Cannot retrieve Chrome location: ${error.message}`),
-		);
-	}
-
-	if (verbose) {
-		logger.info(`Browser path: ${stdout}`);
-	}
-
-	console.info(stdout.trim());
+	const executablePath = await getBrowserExecutablePath(verbose);
+	const options = createOptions(config);
 
 	const browser = await puppeteer.launch({
-		executablePath: stdout.trim(),
+		executablePath: executablePath,
 		// https://www.howtogeek.com/devops/how-to-run-puppeteer-and-headless-chrome-in-a-docker-container/#using-puppeteer-in-docker
 		args: [
 			"--disable-gpu",
@@ -62,15 +41,6 @@ export async function requestResult(
 		],
 		defaultViewport: null,
 	});
-
-	const options: Options = {
-		ci: DEFAULT_OPTIONS.ci,
-		device: config.device ?? DEFAULT_OPTIONS.device,
-		language: config.language ?? DEFAULT_OPTIONS.language,
-		max_tab: DEFAULT_OPTIONS.max_tab,
-		retry: config.retry ?? DEFAULT_OPTIONS.retry,
-		timeout: config.timeout ?? DEFAULT_OPTIONS.timeout,
-	};
 
 	// As the createJsonReports use console.* functions to display progress info and errors and do not send back these information,
 	// so we need to disable the console.* functions during this operation to handle the output ourselves.
@@ -131,5 +101,46 @@ export async function requestResult(
 		console.log = consoleLog;
 		console.error = consoleError;
 		await browser.close();
+	}
+}
+
+function createOptions(config: GreenITConfig): Options {
+	return {
+		ci: DEFAULT_OPTIONS.ci,
+		device: config.device ?? DEFAULT_OPTIONS.device,
+		language: config.language ?? DEFAULT_OPTIONS.language,
+		max_tab: DEFAULT_OPTIONS.max_tab,
+		retry: config.retry ?? DEFAULT_OPTIONS.retry,
+		timeout: config.timeout ?? DEFAULT_OPTIONS.timeout,
+	};
+}
+
+/**
+ * Get the browser executable path from the puppeteer dependency of the greenit-cli dependency
+ *
+ * @see {@link https://github.com/puppeteer/puppeteer/issues/679#issuecomment-1274988821}
+ */
+async function getBrowserExecutablePath(verbose: boolean): Promise<string> {
+	try {
+		const { stdout } = await execa({
+			cwd: join(__dirname, "../../node_modules/greenit-cli"),
+			encoding: "utf8",
+		})`node ${["-e", "console.log(require('puppeteer').executablePath())"]}`;
+
+		if (verbose) {
+			logger.info(`Browser path: ${stdout}`);
+		}
+
+		return stdout;
+	} catch (error) {
+		if (typeof error === "string") {
+			return Promise.reject(new GreenITError(error));
+		}
+
+		if (error instanceof Error) {
+			return Promise.reject(new GreenITError(error.message));
+		}
+
+		return Promise.reject(error);
 	}
 }
