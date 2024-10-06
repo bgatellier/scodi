274faabacb6be0296e35e21de50fef2848fe437c
Bastien Gatellier <contact@bgatellier.fr>
2024-10-06 15:42:09 +0200

    refactor: improve maintainability and reliability

--- packages/observatory/src/error/ObservatoryError.ts
+++ packages/observatory/src/error/ObservatoryError.ts
@@ -1,8 +1,6 @@
-import type { ObservatoryScanState } from "@scodi/common";
-
 export class ObservatoryError extends Error {
-	constructor(state: ObservatoryScanState) {
-		super(state);
+	constructor(message?: string, options?: ErrorOptions) {
+		super(message, options);
 
 		// @see {@link https://github.com/Microsoft/TypeScript-wiki/blob/main/Breaking-Changes.md#extending-built-ins-like-error-array-and-map-may-no-longer-work}
 		Object.setPrototypeOf(this, ObservatoryError.prototype);
