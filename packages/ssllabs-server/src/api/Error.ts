41889ad9bfa626232c7a11d8a0a47971b2109fe6
Bastien Gatellier <contact@bgatellier.fr>
2024-10-06 15:49:48 +0200

    refactor: improve reliability

--- packages/ssllabs-server/src/api/Error.ts
+++ packages/ssllabs-server/src/api/Error.ts
@@ -1,14 +1,14 @@
 /**
  * @see [Documentation]{@link https://github.com/ssllabs/ssllabs-scan/blob/master/ssllabs-api-docs-v3.md#error-reporting}
  */
-export interface SsllabsServerError {
+export interface SsllabsServerApiError {
 	errors: {
 		field: string;
 		message: string;
 	}[];
 }
 
-export const isSsllabsServerError = (
+export const isSsllabsServerApiError = (
 	object: unknown,
-): object is SsllabsServerError =>
-	Array.isArray((object as SsllabsServerError).errors);
+): object is SsllabsServerApiError =>
+	Array.isArray((object as SsllabsServerApiError).errors);
