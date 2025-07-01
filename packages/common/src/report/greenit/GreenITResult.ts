f88a41d679eeddea4903b1dedd95baea1a559f5e
Bastien Gatellier <contact@bgatellier.fr>
2025-07-01 18:49:45 +0200

    chore: upgrade greenit-analysis-cli & lighthouse
    chore: coucou2

--- packages/common/src/report/greenit/GreenITResult.ts
+++ packages/common/src/report/greenit/GreenITResult.ts
@@ -1,4 +1,4 @@
-import type { GeneralProperties, KPIProperties } from "greenit-cli";
+import type { GeneralProperties, KPIProperties } from "greenit-analysis-cli";
 import type { Result } from "../Result.js";
 
 export type GreenITResult = Result & GeneralProperties & KPIProperties;
