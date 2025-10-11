b6656c6f9e465b30bddcf28723b4de0ceb76426a
Bastien Gatellier <contact@bgatellier.fr>
2025-10-11 14:57:53 +0200

    feat: use API v2

--- packages/common/src/report/observatory/ObservatoryResult.ts
+++ packages/common/src/report/observatory/ObservatoryResult.ts
@@ -1,8 +1,4 @@
 import type { Result } from "../Result.js";
 import type { ObservatoryScan } from "./model/ObservatoryScan.js";
-import type { ObservatoryTests } from "./model/ObservatoryTests.js";
 
-export type ObservatoryResult = Result & {
-	scan: ObservatoryScan;
-	tests: ObservatoryTests;
-};
+export type ObservatoryResult = Result & ObservatoryScan;
