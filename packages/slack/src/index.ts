ce4f7bbe37c20fd8f6e0211a27c90d767dc7fce2
Bastien Gatellier <contact@bgatellier.fr>
2025-10-19 18:22:30 +0200

    chore: remove the Slack package

--- packages/slack/src/index.ts
+++
@@ -1,9 +0,0 @@
-import type { ModuleIndex, ModuleMetadata } from "@scodi/common";
-import { SlackModule } from "./SlackModule.js";
-
-export const initialize: ModuleIndex["initialize"] = (
-	moduleMetadata: ModuleMetadata,
-	verbose: boolean,
-) => {
-	return new SlackModule(moduleMetadata, verbose);
-};
