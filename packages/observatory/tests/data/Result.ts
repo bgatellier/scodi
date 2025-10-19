ac981c46f75fb8479e02ee639ee538c9ac2632b6
Bastien Gatellier <contact@bgatellier.fr>
2025-10-19 13:08:03 +0200

    test: fix test

--- packages/observatory/tests/data/Result.ts
+++ packages/observatory/tests/data/Result.ts
@@ -1,32 +1,19 @@
-import { type ObservatoryReport, ObservatoryScanState } from "@scodi/common";
+import type { ObservatoryConfig, ObservatoryReport } from "@scodi/common";
 
-export const RESULT: ObservatoryReport["result"] = {
-	scan: {
-		end_time: "May 13, 2022 5:58 PM",
+export function generateMockResult(
+	config: ObservatoryConfig,
+): ObservatoryReport["result"] {
+	return {
+		id: 77666718,
+		details_url: `https://developer.mozilla.org/en-US/observatory/analyze?host=${config.host}`,
+		algorithm_version: 4,
+		scanned_at: "2024-08-12T08:20:18.926Z",
+		error: null,
 		grade: "B",
-		hidden: true,
-		response_headers: {},
-		scan_id: 1,
 		score: 95,
-		likelihood_indicator: "",
-		start_time: "",
-		state: ObservatoryScanState.FINISHED,
-		tests_failed: 3,
-		tests_passed: 4,
-		tests_quantity: 12,
-	},
-	tests: {
-		"content-security-policy": {
-			expectation: "csp-implemented-with-no-unsafe",
-			name: "content-security-policy",
-			output: {
-				data: {},
-			},
-			pass: false,
-			result: "csp-implemented-with-unsafe-inline-in-style-src-only",
-			score_description:
-				"Content Security Policy (CSP) implemented with unsafe-inline inside style-src directive",
-			score_modifier: -5,
-		},
-	},
-};
+		status_code: 200,
+		tests_failed: 0,
+		tests_passed: 10,
+		tests_quantity: 10,
+	};
+}
